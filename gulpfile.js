541e622c29a9216df3aa7d733d8b5e6779b707bf
Gastón Lasarte <gastonlasarte@gmail.com>
2016-01-26 22:25:57 -0300

    master: Created new task for watch sass files

--- gulpfile.js
+++ gulpfile.js
@@ -3,8 +3,14 @@ var gulp = require('gulp');
 // Requires the gulp-sass plugin
 var sass = require('gulp-sass');
 
+// Sass tasks that compiles SASS to CSS
 gulp.task('sass', function () {
 	return gulp.src('app/scss/**/*.scss') // Gets all files ending with .scss in app/scss and children dirs
 	.pipe(sass()) // Using the gulp sass plugin
 	.pipe(gulp.dest('app/css'))
+});
+
+// Watchers
+gulp.task('watch', function () {
+	gulp.watch('app/scss/**/*.scss', ['sass']); // Watch all sass files
 });
