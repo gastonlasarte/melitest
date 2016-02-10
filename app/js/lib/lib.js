9b6b01b0f3314cb1dc196b5490e4153b3c48c3be
Gastón Lasarte <gastonlasarte@gmail.com>
2016-02-10 02:32:40 -0300

    master: Finished initial markup. Added gitignore file for dist folder

--- app/js/lib/lib.js
+++
@@ -1,1 +0,0 @@
-console.log("Otro amiguito ha aparecido");
--- app/js/main.js
+++ app/js/main.js
@@ -1,1 +1,11 @@
-console.log('Hola hi');
+// Carousel
+/*var carousel = $('.myCarousel').carousel({
+   "pagination" : true,
+   "limitPerPage" : 3
+});*/
+function qS(selector) { return document.querySelector(selector); };
+var carousel = new ch.Carousel(qS('.myCarousel'), 
+	{
+		"limitPerPage" : 3,
+		"pagination" : true
+});
