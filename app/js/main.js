8302618bc8100eec15094b33968189119b41b944
Gastón Lasarte <gastonlasarte@gmail.com>
2016-02-10 02:42:21 -0300

    master: Deleted extra code

--- app/js/main.js
+++ app/js/main.js
@@ -1,8 +1,3 @@
-// Carousel
-/*var carousel = $('.myCarousel').carousel({
-   "pagination" : true,
-   "limitPerPage" : 3
-});*/
 function qS(selector) { return document.querySelector(selector); };
 var carousel = new ch.Carousel(qS('.myCarousel'), 
 	{
